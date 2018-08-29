b5994ea1261c39cce79ae54e0cd4b35e6d658c8c
Oleg Shilov <shilov-1@yandex.ru>
2018-08-29 10:57:37 +0300

    redirect with router;

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -4,12 +4,17 @@ import MenuAndPopup from 'components/menu-and-popup/menuAndPopup';
 import Bg from 'components/bg/Bg';
 import routePropsShape from 'assets/helpers/routePropsShape';
 import './main.scss';
+import redirectWithRouter from 'assets/helpers/redirectWithRouter';
 
 export default class Main extends React.Component {
   constructor(routeProps) {
     super(routeProps);
 
     this.routeProps = routeProps;
+  }
+
+  componentDidMount() {
+    redirectWithRouter(this.routeProps);
   }
 
   render() {
